10d003ab71eec9556d53e378e3f7a1524f848767
taichi.n <taichi6930.tokyojihen.pelusa@gmail.com>
2021-07-03 22:06:24 +0900

    ファイルの移管

--- ABC/ABC_198_C.py
+++
@@ -1,5 +0,0 @@
-import math
-r, x, y = map(int, input().split())
-
-a = (x ** 2 + y ** 2) ** 0.5
-print(2 if a < r else int(math.ceil(a / r)))
--- AGC/test.py
+++
@@ -1,5 +0,0 @@
-a = int(input())
-b = input()
-c = map(int, input().split())
-d = list(map(int, input().split()))
-e = [int(input()) for _ in range(a)]
