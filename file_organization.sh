6e7975a2d8bb33f24727ce0d158ffa1c0f8a2f87
taichi.n <taichi6930.tokyojihen.pelusa@gmail.com>
2021-07-07 14:51:37 +0900

    コミット

--- file_organization.sh
+++ file_organization.sh
@@ -1,7 +1,11 @@
 files="./*"
-ignoreFiles=("archive", "makestr.txt", "makestr.py", "temp.py")
+ignoreFiles=("archive", "makestr.txt", "makestr.py", "temp.py", "file_organization.sh")
 for filepath in $files; do
-    if [ ${filepath:2} = "archive" -o ${filepath:2} = "makestr.txt" -o ${filepath:2} = "makestr.py" -o ${filepath:2} = "temp.py" ]; then
+    if [ ${filepath:2} = "archive" \
+    -o ${filepath:2} = "makestr.txt" \
+    -o ${filepath:2} = "makestr.py" \
+    -o ${filepath:2} = "temp.py" \
+    -o ${filepath:2} = "file_organization.sh" ]; then
         continue;
     fi
     modifyfilepath="./archive/"${filepath:2}
