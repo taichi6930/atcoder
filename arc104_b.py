2b9632a301df57f5566b0037db730a0caa60287e
taichi.n <taichi6930.tokyojihen.pelusa@gmail.com>
2021-08-13 19:46:03 +0900

    追加

---
+++ arc104_b.py
@@ -0,0 +1,102 @@
+from bisect import bisect_left
+from itertools import permutations  # 順列全探索で使う
+from itertools import accumulate  # 累積和を求めるときに使う
+import math
+import collections
+from operator import mul
+from functools import reduce
+
+mod = 10 ** 9 + 7
+alphaList = list("abcdefghijklmnopqrstuvwxyz")
+mod2 = 998244353
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+
+    for k in range(2, int(math.sqrt(n)) + 1):
+        if n % k == 0:
+            return False
+
+    return True
+
+
+def make_divisors(n):
+    """
+    約数列挙を行う。
+
+    Parameters
+    ----------
+    n : int
+        約数を求めたい数
+
+    Returns
+    -------
+    divisors : [int]
+        約数が昇順に入った配列
+    """
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    divisors = lower_divisors + upper_divisors[::-1]
+    return divisors
+
+
+def prime_factorization(n):
+    """
+    task:prime factorization
+    return:prime
+    type:list
+    """
+    lis = []
+    for i in range(2, int(n ** 0.5) + 1):
+        while True:
+            if n % i == 0:
+                lis.append(i)
+                n = n // i
+
+            else:
+                break
+
+    if n > int(n ** 0.5):
+        lis.append(n)
+
+    return lis
+
+
+def str2intWithArray(Array):
+    return list(map(lambda x: int(x), Array))
+
+
+def int2strWithArray(Array):
+    return list(map(lambda x: str(x), Array))
+
+
+def cmb(n, r):
+    r = min(n-r, r)
+    if r == 0:
+        return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1, r + 1))
+    return over // under
+
+
+def main():
+    n, s = map(str, input().split())
+    n = int(n)
+    t = "" + s
+    print(t)
+
+    t = t.replace('A', '1').replace('G', '2').replace('C', '3').replace(
+        'T', '4').replace('1', 'T'). replace('2', 'C').replace('3', 'G').replace('4', 'A')
+    print(s, t)
+
+
+if __name__ == '__main__':
+    main()
