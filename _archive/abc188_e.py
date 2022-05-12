e1d3371565af9cf3805e837ac6e6b361279f23a3
T N <taichi6930.tokyojihen.pelusa@gmail.com>
2022-05-12 21:21:54 +0900

    追加

--- _archive/abc188_e.py
+++ _archive/abc188_e.py
@@ -1,3 +1,8 @@
 n, m = map(int, input().split())
 A = list(map(int, input().split()))
-XY = [[]for _ in range(n)]
+XY = sorted([list(map(int, input().split()))
+            for _ in range(m)], key=lambda x: (x[1], x[0]))
+goldList = [1] + [0] * (n - 1)
+silverList = [0] * n
+
+print(silverList)
--- _archive/dp_d.py
+++ _archive/dp_d.py
@@ -1,19 +1,19 @@
-from pprint import pprint
-N, W = map(int, input().split())
-dp = [[0 for _ in range(W + 1)] for _ in range(N)]
+n, W = map(int, input().split())
+WV = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(N):
-    w, v = map(int, input().split())
-    dp[i][w] = v
+dp = [[0 for _ in range(W + 1)] for _ in range(n)]
+
+for i, [w, v] in enumerate(WV):
+    dp[i][w] = max(v, dp[i][w])
     if i == 0:
         continue
-    for j in range(1, W + 1):
-        if dp[i - 1][j] == 0:
-            continue
-        if j + w > W:
-            dp[i][j] = dp[i - 1][j]
-            continue
-        # dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+    for j in range(W + 1):
+        k1 = dp[i - 1][j]
+        k2 = 0
+
+        if 0 <= j - w:
+            k2 = dp[i - 1][j - w] + v
+        dp[i][j] = max(k1, k2)
 
 
-pprint(dp)
+print(max(dp[-1]))
